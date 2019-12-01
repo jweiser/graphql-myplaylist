9b08120d49dbf9570dce95e767dbb97ee7a5a597
Joel Weiser <joel.weiser@oicr.on.ca>
2019-12-01 14:11:01 -0500

    updates the Book id to be of type GraphQLID

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,7 +4,8 @@ const _ = require('lodash');
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLID
 } = graphql;
 
 // dummy data
@@ -17,7 +18,7 @@ var books = [
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
@@ -28,7 +29,7 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: { id: { type: GraphQLString }},
+            args: { id: { type: GraphQLID }},
             resolve(parent, args) {
                 return _.find(books, {id: args.id });
             }
