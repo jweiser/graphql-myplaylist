df6fdc84dc0bd7f2269647a422cceb2248993ae8
Joel Weiser <joel.weiser@oicr.on.ca>
2019-12-01 13:44:33 -0500

    updates app.js to use the GraphQL schema file

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema')
 
 const app = express();
 
+app.use('/graphql', graphqlHTTP({
+    schema
+}));
 
 const port = 4000;
 app.listen(port, () => {
