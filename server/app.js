4b44e707042b9c4f2c6f3a132160bcdbc24a6008
Joel Weiser <joel.weiser@oicr.on.ca>
2019-12-04 22:45:31 -0500

    adds mongoose connection to app.js

--- server/app.js
+++ server/app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema')
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const connectionString = require('./secrets');
 
 const app = express();
+
+mongoose.connect(
+    connectionString,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+mongoose.connection.once('open', () => {
+    console.log(`connected to database`);
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
